f58e89616091c0a8587c01423c3f7cca46c24cd6
JustINCodingUK <coderstudent09@gmail.com>
2025-08-24 16:13:55 +0530

    Implement completion feature; Fix #2

--- lib/ui/assignment_card.dart
+++ lib/ui/assignment_card.dart
@@ -19,78 +19,81 @@ class AssignmentCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    final buttonLabel = assignment.isCompleted ? "Mark\nIncomplete" : "Mark\nComplete";
+    final buttonLabel = assignment.isCompleted
+        ? "Mark\nIncomplete"
+        : "Mark\nComplete";
 
     return Card.filled(
-        shape: !assignment.isCompleted ? RoundedRectangleBorder(
-          side: BorderSide(
-            color: Theme.of(context).colorScheme.primary,
-            width: 4,
-          ),
-          borderRadius: BorderRadius.circular(32),
-        ) : null,
-        elevation: 16,
-        child: InkWell(
-          onTap: onClick,
-          child: Stack(
-            children: [
-              Align(
-                alignment: Alignment.topLeft,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      assignment.subject,
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-
-                    SizedBox(height: 16),
-
-                    Text(
-                      assignment.title,
-                      style: Theme.of(context).textTheme.headlineMedium,
-                    ),
-
-                    SizedBox(height: 16),
-
-                    Text("Due", style: Theme.of(context).textTheme.bodyMedium),
-
-                    SizedBox(height: 8),
-
-                    Text(
-                      assignment.dueDate.date(),
-                      style: Theme.of(
-                        context,
-                      ).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold),
-                    ),
-
-                    SizedBox(height: 16),
-
-                    Text("Files", style: Theme.of(context).textTheme.bodyMedium),
-
-                    Text(
-                      assignment.attachments.length.toString(),
-                      style: Theme.of(
-                        context,
-                      ).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ).pad(16),
+      shape: !assignment.isCompleted
+          ? RoundedRectangleBorder(
+              side: BorderSide(
+                color: Theme.of(context).colorScheme.primary,
+                width: 4,
               ),
+              borderRadius: BorderRadius.circular(32),
+            )
+          : RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+      elevation: 16,
+      child: InkWell(
+        onTap: onClick,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    assignment.subject,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
 
-              Positioned(
-                bottom: 0,
-                right: 0,
-                child: ElevatedButton(
-                  onPressed: onCompletionMarked,
-                  child: Text(buttonLabel, textAlign: TextAlign.center).pad(8),
-                ).pad(16),
-              ),
-            ],
-          ).pad(8),
-        ),
-      );
+                  SizedBox(height: 16),
+
+                  Text(
+                    assignment.title,
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+
+                  SizedBox(height: 16),
+
+                  Text("Due", style: Theme.of(context).textTheme.bodyMedium),
+
+                  SizedBox(height: 8),
+
+                  Text(
+                    assignment.dueDate.date(),
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  SizedBox(height: 16),
+
+                  Text("Files", style: Theme.of(context).textTheme.bodyMedium),
+
+                  Text(
+                    assignment.attachments.length.toString(),
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ).pad(16),
+            ),
+
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: ElevatedButton(
+                onPressed: onCompletionMarked,
+                child: Text(buttonLabel, textAlign: TextAlign.center).pad(8),
+              ).pad(16),
+            ),
+          ],
+        ).pad(8),
+      ),
+    );
   }
 }
