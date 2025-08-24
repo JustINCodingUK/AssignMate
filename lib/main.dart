367c2cc0c9cab8848da0e6247420436b2475cf43
JustINCodingUK <coderstudent09@gmail.com>
2025-08-24 23:50:51 +0530

    Provide RemindersRepository before MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,14 @@ import 'package:assignmate/firebase_options.dart';
 import 'package:assignmate/nav.dart';
 import 'package:assignmate/network/firestore_client.dart';
 import 'package:assignmate/notifications/fcm_notifications.dart';
-import 'package:assignmate/notifications/local_notifications.dart';
 import 'package:assignmate/theme/theme.dart';
 import 'package:assignmate/theme/util.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'data/reminders_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,30 +24,24 @@ void main() async {
   );
   final appDb = await getDatabase();
 
-  final fcmNotificationManager = FCMNotificationManager();
+  final fcmNotificationManager = FCMNotificationManager.get();
 
   await fcmNotificationManager.checkPermission();
   fcmNotificationManager.registerBackgroundCallback();
 
-  runApp(
-    AssignmateApplication(db: appDb, notificationManager: fcmNotificationManager),
-  );
+  runApp(AssignmateApplication(db: appDb));
 }
 
 class AssignmateApplication extends StatelessWidget {
   final AppDatabase db;
-  final FCMNotificationManager notificationManager;
 
   const AssignmateApplication({
     super.key,
-    required this.db,
-    required this.notificationManager,
+    required this.db
   });
 
   @override
   Widget build(BuildContext context) {
-
-    notificationManager.registerForegroundCallback(context);
 
     final brightness = View.of(context).platformDispatcher.platformBrightness;
     final textTheme = createTextTheme(context, "Nunito", "Montserrat");
@@ -54,15 +49,26 @@ class AssignmateApplication extends StatelessWidget {
 
     return BlocProvider<AuthBloc>(
       create: (context) => AuthBloc()..add(AuthCheckEvent()),
-      child: RepositoryProvider<AssignmentsRepository>(
-        create: (context) => AssignmentsRepository(
-          FirestoreClient(),
-          AttachmentRepository(
-            FirestoreClient(),
-            context.read<AuthBloc>().googleApiClient,
+      child: MultiRepositoryProvider(
+        providers: [
+          RepositoryProvider(
+            create: (context) => AssignmentsRepository(
+              FirestoreClient(),
+              AttachmentRepository(
+                FirestoreClient(),
+                context.read<AuthBloc>().googleApiClient,
+              ),
+              db,
+            ),
           ),
-          db,
-        ),
+
+          RepositoryProvider(
+            create: (context) => RemindersRepository(
+              db: db,
+              firestoreClient: FirestoreClient(),
+            ),
+          )
+        ],
         child: MaterialApp.router(
           theme: brightness == Brightness.light ? theme.light() : theme.dark(),
           routerConfig: router,
