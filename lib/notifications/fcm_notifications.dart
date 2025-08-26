f67232abd3d8aca16af990643bd2405206b92785
JustINCodingUK <coderstudent09@gmail.com>
2025-08-27 02:20:54 +0530

    Implement background deletion, editing, and creation

--- lib/notifications/fcm_notifications.dart
+++ lib/notifications/fcm_notifications.dart
@@ -2,7 +2,9 @@ import 'package:assignmate/bloc/assignments_bloc.dart';
 import 'package:assignmate/bloc/events/assignment_event.dart';
 import 'package:assignmate/data/assignment_repository.dart';
 import 'package:assignmate/data/attachment_repository.dart';
+import 'package:assignmate/data/reminders_repository.dart';
 import 'package:assignmate/db/database.dart';
+import 'package:assignmate/model/reminder.dart';
 import 'package:assignmate/network/firestore_client.dart';
 import 'package:assignmate/network/google_api_client.dart';
 import 'package:assignmate/notifications/local_notifications.dart';
@@ -13,20 +15,27 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 Future<void> _handleFcmPayload(RemoteMessage message) async {
   final db = await getDatabase();
 
-  final attachmentRepository = AttachmentRepository(
-    FirestoreClient(),
-    GoogleApiClient(),
-  );
-  final assignmentRepository = AssignmentsRepository(
-    FirestoreClient(),
-    attachmentRepository,
-    db,
-  );
   final payload = message.data;
 
-  switch(payload["action"]) {
-    case "create": _saveAssignment(payload["id"], assignmentRepository);
-    // TODO: After merge with feature/edit
+  if(payload["action"]=="reminder") {
+    final reminderRepository = RemindersRepository(db: db, firestoreClient: FirestoreClient());
+    await reminderRepository.saveReminder(payload["id"]);
+  } else {
+    final attachmentRepository = AttachmentRepository(
+      FirestoreClient(),
+      GoogleApiClient(),
+    );
+    final assignmentRepository = AssignmentsRepository(
+      FirestoreClient(),
+      attachmentRepository,
+      db,
+    );
+
+    switch(payload["action"]) {
+      case "create": await _saveAssignment(payload["id"], assignmentRepository);
+      case "edit": await _editAssignment(payload["id"], assignmentRepository);
+      case "delete": await _deleteAssignment(payload["id"], assignmentRepository);
+    }
   }
 }
 
@@ -41,6 +50,26 @@ Future<void> _saveAssignment(String id, AssignmentsRepository assignmentReposito
     assignment.title,
     assignment.dueDate,
   );
+}
+
+Future<void> _editAssignment(String id, AssignmentsRepository assignmentRepository) async {
+  final updatedAssignment = await assignmentRepository.getAssignment(id);
+  
+  await assignmentRepository.updateLocalAssignment(updatedAssignment);
+
+  final localNotificationManager = await LocalNotificationManager.get();
+  await localNotificationManager.scheduleNotification(
+    updatedAssignment.id.hashCode & 0x7FFFFFFF,
+    "New Assignment",
+    updatedAssignment.title,
+    updatedAssignment.dueDate,
+  );
+}
+
+Future<void> _deleteAssignment(String id, AssignmentsRepository assignmentRepository) async {
+  await assignmentRepository.deleteAssignment(id);
+  final localNotificationManager = await LocalNotificationManager.get();
+  await localNotificationManager.cancelNotification(id.hashCode & 0x7FFFFFFF);
 }
 
 class FCMNotificationManager {
